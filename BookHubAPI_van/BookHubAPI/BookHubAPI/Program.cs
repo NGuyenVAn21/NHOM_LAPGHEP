5127af85cc851849531d12dc6709cb4d81fdf6f6
NGuyenVAn21 <dinhvanparkour@gmail.com>
2025-12-25 11:24:27 +0700

    Update API controllers and admin interface, remove build files

--- BookHubAPI_van/BookHubAPI/BookHubAPI/Program.cs
+++ BookHubAPI_van/BookHubAPI/BookHubAPI/Program.cs
@@ -1,5 +1,5 @@
-using BookHubAPI.Models;
-
+﻿using BookHubAPI.Models;
+using Microsoft.EntityFrameworkCore;  // ← DÒNG NÀY BẮT BUỘC
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
